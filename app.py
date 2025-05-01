58d5b05a27549b7e85174ec5594fdbce0be71237
Divya3107788 <divyabaskar8623@gmail.com>
2025-05-02 01:28:07 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Sample in-memory data store
+appointments = []
+
+# Route to view all appointments
+@app.route('/appointments', methods=['GET'])
+def get_appointments():
+    return jsonify(appointments)
+
+# Route to add a new appointment
+@app.route('/appointments', methods=['POST'])
+def add_appointment():
+    data = request.get_json()  # Get the data sent in the request
+    appointment = {
+        'name': data['name'],
+        'date': data['date'],
+        'time': data['time'],
+        'service': data['service']
+    }
+    appointments.append(appointment)  # Add the new appointment to the list
+    return jsonify({'message': 'Appointment added successfully!', 'appointment': appointment}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
